8b9c6ccf93357c8267bc927d8a6c5a3b5bb35b5b
Kaustab Pal <kaustab21@gmail.com>
2023-06-02 18:53:04 +0530

    added template_model.py

--- src_dir/models/template_model.py
+++ src_dir/models/template_model.py
@@ -8,4 +8,54 @@ import torch.nn as nn
 import torch.nn.functional as F
 import yaml
 import time
-from spf.models.base import BasePredictionModel
+from src_dir.models.base import BaseModel
+
+
+class GenericModel(BaseModel):
+    def __init__(self, cfg):
+        """Init all layers needed for range image-based point cloud prediction"""
+        super().__init__(cfg)
+
+    def forward(self, x):
+        """Forward range image-based point cloud prediction
+
+        Args:
+            x (torch.tensor): Input tensor of concatenated, unnormalize range images
+
+        Returns:
+            dict: Containing the predicted range tensor and mask logits
+        """
+        pass
+
+
+
+class Normalization(nn.Module):
+    """Custom Normalization layer to enable different normalization strategies"""
+
+    def __init__(self, cfg, n_channels):
+        """Init custom normalization layer"""
+        super(Normalization, self).__init__()
+        self.cfg = cfg
+        self.norm_type = self.cfg["MODEL"]["NORM"]
+        n_channels_per_group = self.cfg["MODEL"]["N_CHANNELS_PER_GROUP"]
+
+        if self.norm_type == "batch":
+            self.norm = nn.BatchNorm3d(n_channels)
+        elif self.norm_type == "instance":
+            self.norm = nn.InstanceNorm3d(n_channels)
+        elif self.norm_type == "none":
+            self.norm = nn.Identity()
+
+    def forward(self, x):
+        """Forward normalization pass
+
+        Args:
+            x (torch.tensor): Input tensor
+
+        Returns:
+            torch.tensor: Output tensor
+        """
+        x = self.norm(x)
+        return x
+
+
